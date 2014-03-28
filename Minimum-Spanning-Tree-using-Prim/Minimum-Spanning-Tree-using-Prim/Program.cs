be94adf56fb883865aa5f3669ce0c914eaf4ff5c
mikevlaar <udgement1>
2014-03-28 20:17:12 +0100

    with all classes

---
+++ Minimum-Spanning-Tree-using-Prim/Minimum-Spanning-Tree-using-Prim/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Spanning_Tree_using_Prim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[][] matrix = new int[10][];
+ 
+            for (int i = 0; i < matrix.Length; i++) {
+                 matrix[i] = new int[i + 1];
+             }
+ 
+            for (int i = 0; i < matrix.Length; i++) {
+                 for (int j = 0; j < i; j++) {
+                     matrix[i][j] = (int) (random.Next(0, 100));
+                 }
+             }
+ 
+            Console.WriteLine("Graph is >>");
+             for (int i = 0; i < matrix.Length; i++) {
+                 for (int j = 0; j < i; j++) {
+                     Console.Write(matrix[i][j] + ",");
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine();
+
+             Graph graph = new Graph(matrix);
+             Console.WriteLine("The Total Cost is : " + graph.performPrim(0));
+             Console.ReadLine();
+        }
+    }
+}
